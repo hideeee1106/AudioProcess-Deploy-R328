5c2f27ec6438a9b974f477f836f98f55d1435e9c
hideeee <hideeee1106@gmail.com>
2025-03-28 17:28:32 +0800

    build_r328.sh

---
+++ build_R328.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# build
+BUILD_DIR=./build_r328/
+
+if [[ ! -d "${BUILD_DIR}" ]]; then
+  mkdir -p ${BUILD_DIR}
+fi
+
+#GCC_COMPILER=/root/arm-rockchip830-linux-uclibcgnueabihf/bin/arm-rockchip830-linux-uclibcgnueabihf
+
+cd ${BUILD_DIR}
+cmake .. \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DENABLE_RKNN2=OFF \
+    -DENABLE_MNN=ON\
+    -DENABLE_TIME_MONITOR=OFF \
+#    -DCMAKE_C_COMPILER=${GCC_COMPILER}-gcc \
+#    -DCMAKE_CXX_COMPILER=${GCC_COMPILER}-g++
+make -j6
+
+make install
+
+cd -
